42184015aa66420679d3953081b54dd58a2f4723
Pheuw1 <gabrieldelaflotte@gmail.com>
2023-05-30 18:02:22 +0200

    ---

--- performance.py
+++ performance.py
@@ -1,8 +1,6 @@
-#open data.csv using pandas
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
-#read data.csv
 
 def calculate_mse(y_true, y_pred):
     n = len(y_true)
@@ -17,34 +15,34 @@ def calculate_r2_score(y_true, y_pred):
     return r2
 
 
-data = pd.read_csv("data.csv")
-
-x  = data["km"].values.reshape(-1, 1)
-y_true  = data["price"].values.reshape(-1, 1)
-
-try:
-    theta = np.load("trained_theta.npy")
-except:
-    print("No trained_theta.npy found. Please run ft_linear_regression.py first.")
-    exit()
+def print_metrics(title ,y_true, y_pred):
+    mse = calculate_mse(y_true, y_pred)
+    r2 = calculate_r2_score(y_true, y_pred)
+    print(f"Metrics for {title}:")
+    print("Mean Squared Error (MSE):", mse)
+    print("Coefficient of Determination (R-squared):", r2)
     
-print("theta : ",theta)
-    
-y_pred = theta[0] + (theta[1] * x)
+def main():
+    data = pd.read_csv("data.csv")
 
-mse = calculate_mse(y_true, y_pred)
-r2 = calculate_r2_score(y_true, y_pred)
-print("Metrics for trained_theta.npy:")
-print("Mean Squared Error (MSE):", mse)
-print("Coefficient of Determination (R-squared):", r2)
+    x  = data["km"].values.reshape(-1, 1)
+    y_true  = data["price"].values.reshape(-1, 1)
 
+    theta = np.zeros(2)
+    try:
+        theta = np.load("trained_theta.npy")
+    except:
+        print("No trained_theta.npy found. Please run ft_linear_regression.py first.")
+        exit()
+    print("theta : ",theta)
+        
+    y_pred = theta[0] + (theta[1] * x)
+    print_metrics("trained theta", y_true, y_pred)
 
-model = LinearRegression()
-model.fit(x, y_true)
+    model = LinearRegression()
+    model.fit(x, y_true)
+    y_pred = model.predict(x)
+    print_metrics("sklearn", y_true, y_pred)
 
-y_pred = model.predict(x)
-mse = calculate_mse(y_true, y_pred)
-r2 = calculate_r2_score(y_true, y_pred)
-print("Metrics for scikit LinearRegression:")
-print("Mean Squared Error (MSE):", mse)
-print("Coefficient of Determination (R-squared):", r2)
+if __name__ == '__main__':
+    main()
